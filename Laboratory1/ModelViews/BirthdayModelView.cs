e4b25838d5ba836fe9146315127cc364f986d820
Illia Shevchyk <illia.shevchyk@ukma.edu.ua>
2024-03-01 10:34:59 +0100

    Laboratory 1

---
+++ Laboratory1/ModelViews/BirthdayModelView.cs
@@ -0,0 +1,116 @@
+﻿using KMA.CSharp2024.Laboratory1.Models;
+using KMA.CSharp2024.Laboratory1.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KMA.CSharp2024.Laboratory1.ModelViews
+{
+    internal class BirthdayModelView: INotifyPropertyChanged
+    {
+        private Birthday _birthday = new Birthday();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private RelayCommand<object> _closeCommand;
+
+        public DateTime? BirthdayDate
+        {
+            get
+            {
+                return _birthday.BirthdayDate;
+            }
+            set
+            {
+                _birthday.BirthdayDate = value;
+                ValidateOnBirthdayChange();
+            }
+        }
+
+        public string Age
+        {
+            get 
+            {
+                if (IsValidAge())
+                {
+                    return _birthday.Age.ToString();
+                }
+                return "";
+            }
+        }
+
+        public string ChineseZod
+        {
+            get 
+            {
+                if (IsValidAge())
+                {
+                    return _birthday.ChineseZod.ToString();
+                }
+                return "";
+            }
+        }
+
+        public string WesternZod
+        {
+            get
+            {
+                if (IsValidAge())
+                {
+                    return _birthday.WesternZod.ToString();
+                }
+                return "";
+            }
+        }
+
+        public RelayCommand<object> CloseCommand
+        {
+            get
+            {
+                return _closeCommand ??= new RelayCommand<object>(_ => Environment.Exit(0));
+            }
+        }
+
+        private bool IsValidAge()
+        {
+            return _birthday.Age >= 0 && _birthday.Age <= 135;
+        }
+
+        private bool IsBirthday()
+        {
+            if (_birthday.BirthdayDate == null)
+            {
+                return false;
+            }
+            var today = DateTime.Today;
+            var birth = (DateTime)_birthday.BirthdayDate;
+            return birth.Day == today.Day && birth.Month == today.Month;
+        }
+
+        private void ValidateOnBirthdayChange()
+        {
+            if (!IsValidAge())
+            {
+                MessageBox.Show("Ви ще не народились або вже занадто старі");
+                _birthday.BirthdayDate = null;
+            }
+            if (IsBirthday())
+            {
+                MessageBox.Show("Вітаємо вас з днем народження!");
+            }
+            OnPropertyChanged(nameof(Age));
+            OnPropertyChanged(nameof(WesternZod));
+            OnPropertyChanged(nameof(ChineseZod));
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Laboratory1/Models/Birthday.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMA.CSharp2024.Laboratory1.Models
+{
+    internal class Birthday
+    {
+        public enum ChineseZodiac
+        {
+            Rat, Ox, Tiger, Rabbit, Dragon, Snake, Horse, Goat, Monkey, Rooster, Dog, Pig
+        }
+        public enum WesternZodiac
+        {
+            Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
+        }
+
+        private DateTime? _birthday;
+        private ChineseZodiac _chineseZod;
+        private WesternZodiac _westernZod;
+        private int _age = -1;
+
+        public DateTime? BirthdayDate 
+        {
+            get
+            {
+                return _birthday;
+            }
+            set
+            {
+                _birthday = value;
+                if (_birthday != null)
+                {
+                    Age = CalculateAge((DateTime)_birthday);
+                    ChineseZod = GetChineseZodiac((DateTime)_birthday);
+                    WesternZod = GetWesternZodiac((DateTime)_birthday);
+                }
+            }
+        }
+
+        public ChineseZodiac ChineseZod
+        {
+            get { return _chineseZod; }
+            set { _chineseZod = value; }
+        }
+
+        public WesternZodiac WesternZod
+        {
+            get { return _westernZod; }
+            set { _westernZod = value; }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+
+        public WesternZodiac GetWesternZodiac(DateTime birthday)
+        {
+            int month = birthday.Month;
+            int day = birthday.Day;
+
+            switch (month)
+            {
+                case 1: return day <= 20 ? WesternZodiac.Capricorn : WesternZodiac.Aquarius;
+                case 2: return day <= 19 ? WesternZodiac.Aquarius : WesternZodiac.Pisces;
+                case 3: return day <= 20 ? WesternZodiac.Pisces : WesternZodiac.Aries;
+                case 4: return day <= 20 ? WesternZodiac.Aries : WesternZodiac.Taurus;
+                case 5: return day <= 21 ? WesternZodiac.Taurus : WesternZodiac.Gemini;
+                case 6: return day <= 21 ? WesternZodiac.Gemini : WesternZodiac.Cancer;
+                case 7: return day <= 23 ? WesternZodiac.Cancer : WesternZodiac.Leo;
+                case 8: return day <= 23 ? WesternZodiac.Leo : WesternZodiac.Virgo;
+                case 9: return day <= 23 ? WesternZodiac.Virgo : WesternZodiac.Libra;
+                case 10: return day <= 23 ? WesternZodiac.Libra : WesternZodiac.Scorpio;
+                case 11: return day <= 22 ? WesternZodiac.Scorpio : WesternZodiac.Sagittarius;
+                case 12: return day <= 21 ? WesternZodiac.Sagittarius : WesternZodiac.Capricorn;
+                default: throw new ArgumentOutOfRangeException("Invalid month");
+            }
+        }
+
+        public ChineseZodiac GetChineseZodiac(DateTime birthday) 
+        {
+            int year = birthday.Year;
+            return (ChineseZodiac)((year - 1900) % 12);
+        }
+        public int CalculateAge(DateTime birthDate)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - birthDate.Year;
+            if (birthDate > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
